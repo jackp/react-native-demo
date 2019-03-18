bd9180fe65dd9cdef945a5e10e15b7ccc5f7f287
Jackson Parker <jack@rethinq.io>
2019-03-18 11:47:47 -0400

    [setup] Load graphql endpoint from .env file

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
  */
 
 import React from "react";
+import Config from "react-native-config";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
@@ -12,7 +13,7 @@ import ReservationsScreen from "./components/screens/Reservations";
 // Configure ApolloClient
 // - https://www.apollographql.com/docs/react/essentials/get-started.html#configuration
 const client = new ApolloClient({
-  uri: "GET FROM ENV"
+  uri: Config.GRAPHQL_ENDPOINT
 });
 
 const App = () => (
