637d8232ce10ecdd9179f82c87b9706aee78844b
Jackson Parker <jack@rethinq.io>
2019-03-18 11:38:36 -0400

    [setup] Configure Apollo Client

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,21 @@
  */
 
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 import ReservationsScreen from "./components/screens/Reservations";
 
-const App: React.FunctionComponent = () => <ReservationsScreen />;
+// Configure ApolloClient
+// - https://www.apollographql.com/docs/react/essentials/get-started.html#configuration
+const client = new ApolloClient({
+  uri: "GET FROM ENV"
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <ReservationsScreen />
+  </ApolloProvider>
+);
 
 export default App;
